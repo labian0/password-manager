f1bc1e030ab1c823225834531cf3fa49e58e4d47
labian <labian911@gmail.com>
2022-11-19 17:30:22 +0100

    add pw_to_key

--- manager.py
+++ manager.py
@@ -1,3 +1,9 @@
+from os import urandom
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.fernet import Fernet
+import base64
 class PwManager():
     def __init__(self):
         pass
@@ -10,3 +16,16 @@ class PwManager():
     def createdb(self, path):
         open(path, "x")
         self.opendb(path)
+    
+    def pw_to_key(self, password):
+        backend = default_backend()
+
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=self.salt,
+            iterations=100000,
+            backend=backend
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(bytes(password)))
+        return key
