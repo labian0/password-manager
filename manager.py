b044d41fb677df45857c593bc14da93c7229bb26
labian <labian911@gmail.com>
2022-11-19 15:55:43 +0100

    add opendb and createdb functions

--- manager.py
+++ manager.py
@@ -1,3 +1,10 @@
 class PwManager():
     def __init__(self):
         pass
+
+    def opendb(self, path):
+        self.db = open(path, "r")
+    
+    def createdb(self, path):
+        open(path, "x")
+        self.opendb(path)
