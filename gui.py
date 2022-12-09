ad9546075c7c0662ffd398fcb8e92b3205916e3d
labian <labian911@gmail.com>
2022-12-09 20:04:43 +0100

    remove useless a when adding an entry

--- gui.py
+++ gui.py
@@ -26,7 +26,7 @@ def display_entries():
     for x in man.db.entries:
         name = str(x["name"], "utf-8")
         id = str(x["id"], "utf-8")
-        entry_list.appendChild(f"<a id=\"entry{id}\" onclick=\"detail_entry(\' + {id} + \')\"><li>{name.upper()}</li></a>")
+        entry_list.appendChild(f"<li id=\"entry{id}\" onclick=\"detail_entry(\' + {id} + \')\">{name.upper()}</li>")
 
 def opendatabase():
     man.opendb(file_path_input.getAttributes()["value"], password_input.getAttributes()["value"])
